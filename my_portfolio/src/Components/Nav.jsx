83f21b737834ea5424f510a7a1e5c6e399a82ac1
Jason Carmichael <jasonc0308@icloud.com>
2022-10-26 11:32:24 -0500

    nav nested in header

--- my_portfolio/src/Components/Nav.jsx
+++ my_portfolio/src/Components/Nav.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function Nav () {
     return (
         <div className="nav">
-            <p> NavBar </p>
+            <Link to="/"> <h2> Home </h2></Link>
+            <Link to="/projects"> <h2> Projects </h2></Link>
+            <Link to="/resume"> <h2> Resume </h2></Link>
+            <Link to="/about-me"> <h2> About </h2></Link>
+    
         </div>
     )
 }
