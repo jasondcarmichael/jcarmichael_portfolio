a14da4184a814954bb468cecafec2d0d3834556e
Jason Carmichael <jasonc0308@icloud.com>
2022-10-26 16:17:31 -0500

    added routes to main

--- my_portfolio/src/Components/Main.jsx
+++ my_portfolio/src/Components/Main.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import Projects from './Projects'
+import Resume from './Resume'
+import AboutMe from './AboutMe'
+import Home from './Home'
+
+
 export default function Main () {
     return (
         <div className="main">
+            <Routes>
+                <Route exact path="/" element={ <Home /> } />
+                <Route exact path="projects" element={ <Projects /> } />
+                <Route exact path="resume" element={ <Resume /> } />
+                <Route exact path="about-me" element={ <AboutMe /> } />
+
             <h2> Main Content Here </h2>
+
+            </Routes>
         </div>
     )
 }
